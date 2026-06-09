69d6706d0c82df5b1e167aad510f717e599e836f
Ricardo Zuno <zunosan.ricardo506@gmail.com>
2026-06-08 19:19:17 -0600

    Agregando pruebas para editar y actualizar un presupuesto

---
+++ tests/Feature/Budgets/EditBudgetTest.php
@@ -0,0 +1,53 @@
+<?php
+
+use App\Models\Budget;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
+
+it('allows the owner to view the edit budget form', function () {
+    $user = User::factory()->create([
+        'email_verified_at' => now(),
+    ]);
+
+    $budget = Budget::factory()->for($user)->create([
+        'name' => 'Viaje a las Vegas',
+        'amount' => 1000,
+        'type' => 'general'
+    ]);
+
+    // actingAs auttentica al user
+    $response = $this->actingAs($user)->get(route('budgets.edit', $budget));
+    $response->assertOk();
+    $response->assertSee('Viaje a las Vegas');
+});
+
+it('does not allow guests to view the edit budget form', function () {
+    $user = User::factory()->create([
+        'email_verified_at' => now(),
+    ]);
+
+    $budget = Budget::factory()->for($user)->create();
+
+    //user no autenticado -> redireccionado a login
+    $response = $this->get(route('budgets.edit', $budget));
+    $response->assertRedirect(route('login'));
+});
+
+it('does not allow other users to view the edit budget form', function () {
+    $owner = User::factory()->create([
+        'email_verified_at' => now(),
+    ]);
+
+    $user = User::factory()->create([
+        'email_verified_at' => now(),
+    ]);
+
+    $budget = Budget::factory()->for($owner)->create();
+
+    $response = $this->actingAs($user)->get(route('budgets.edit', $budget));
+    $response->assertForbidden();
+    $response->assertStatus(403);
+    $response->assertSee('No tienes permisos para editar este presupuesto.');
+});
---
+++ tests/Feature/Budgets/UpdateBudgetTest.php
@@ -0,0 +1,142 @@
+<?php
+
+use App\Models\Budget;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
+
+it('allows the owner to update a budget', function () {
+    $user = User::factory()->create([
+        'email_verified_at' => now(),
+    ]);
+
+    $budget = Budget::factory()->for($user)->create([
+        'name' => 'Viaje a las Vegas',
+        'amount' => 1000,
+        'type' => 'general'
+    ]);
+
+    $response = $this->actingAs($user)->put(route('budgets.update', $budget), [
+        'name' => 'Viaje actualizado',
+        'amount' => 1400,
+        'type' => 'general'
+    ]);
+
+    $response->assertRedirect(route('dashboard'));
+    $response->assertSessionHas('success', 'Presupuesto actualizado correctamente.');
+    $this->assertDatabaseHas('budgets', [
+        'id' => $budget->id,
+        'name' => 'Viaje actualizado',
+        'amount' => 1400,
+        'type' => 'general',
+        'user_id' => $user->id
+    ]);
+});
+
+it('validates required fields when updating a budget', function () {
+    $user = User::factory()->create([
+        'email_verified_at' => now(),
+    ]);
+
+    $budget = Budget::factory()->for($user)->create();
+
+    $response = $this->actingAs($user)
+        ->from(route('budgets.edit', $budget))
+        ->put(route('budgets.update', $budget),  [
+            'name' => '',
+            'amount' => '',
+            'type' => '',
+        ]);
+
+    $response->assertRedirect(route('budgets.edit', $budget));
+    $response->assertSessionHasErrors([
+        'name',
+        'amount',
+        'type'
+    ]);
+});
+
+it('validates amount must be greater than zero when updating a budget', function () {
+    $user = User::factory()->create([
+        'email_verified_at' => now(),
+    ]);
+
+    $budget = Budget::factory()->for($user)->create();
+
+    $response = $this->actingAs($user)
+        ->from(route('budgets.edit', $budget))
+        ->put(route('budgets.update', $budget),  [
+            'name' => 'Presupuesto',
+            'amount' => 0,
+            'type' => 'general',
+        ]);
+
+    $response->assertRedirect(route('budgets.edit', $budget));
+    $response->assertSessionHasErrors([
+        'amount',
+    ]);
+});
+
+it('validates type must be valid when updating a budget', function () {
+    $user = User::factory()->create([
+        'email_verified_at' => now(),
+    ]);
+
+    $budget = Budget::factory()->for($user)->create();
+
+    $response = $this->actingAs($user)
+        ->from(route('budgets.edit', $budget))
+        ->put(route('budgets.update', $budget),  [
+            'name' => 'Presupuesto',
+            'amount' => 0,
+            'type' => 'other_general',
+        ]);
+
+    $response->assertRedirect(route('budgets.edit', $budget));
+    $response->assertSessionHasErrors([
+        'type',
+    ]);
+});
+
+it('does not allow guests to update budgets', function () {
+    $user = User::factory()->create([
+        'email_verified_at' => now(),
+    ]);
+
+    $budget = Budget::factory()->for($user)->create();
+
+    $response = $this->put(route('budgets.update', $budget),  [
+        'name' => 'Presupuesto',
+        'amount' => 100,
+        'type' => 'goal',
+    ]);
+
+    $response->assertRedirect(route('login'));
+});
+
+it('does not allow other users to update budgets', function () {
+    $owner = User::factory()->create([
+        'email_verified_at' => now(),
+    ]);
+
+    $otherUser = User::factory()->create([
+        'email_verified_at' => now(),
+    ]);
+
+    $budget = Budget::factory()->for($owner)->create([
+        'name' => 'Presupuesto original'
+    ]);
+
+    $response = $this->actingAs($otherUser)->put(route('budgets.update', $budget), [
+        'name' => 'Hackeado',
+        'amount' => 10000,
+        'type' => 'goal',
+    ]);
+
+    $response->assertForbidden();
+    $this->assertDatabaseHas('budgets', [
+        'id' => $budget->id,
+        'name' => 'Presupuesto original'
+    ]);
+});
